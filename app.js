54a9110f7f8cdf577763ed3b1d9016735e2abae7
DESKTOP-T4APFE5\Work & Study <nikonovmd12@mail.ru>
2023-02-02 19:51:45 +0300

    Управление содержимым

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ var main = function () {
     "use strict";
     $(".tabs a span").toArray().forEach(function (element) {
         $(element).on("click", function() {
+            var $element = $(element);
             $(".tabs a span").removeClass("active");
-            $(element).addClass("active");
+            $element.addClass("active");
             $("main .content").empty();
+            if ($element.parent().is(":nth-child(1)")) {
+                console.log("Щелчок по первой вкладке!")
+            } else if ($element.parent().is(":nth-child(2)")) {
+                console.log("Щелчок по второй вкладке!")
+            } else if ($element.parent().is(":nth-child(3)")) {
+                console.log("Щелчок по третьей вкладке!")
+            }
             return false;
         });
     });
