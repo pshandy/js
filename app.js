a8673a05ff2d10baff178436e2180753811bc636
DESKTOP-T4APFE5\Work & Study <nikonovmd12@mail.ru>
2023-02-02 15:25:27 +0300

    Добавлена работа вкладок, внесены изменения в style.css, list.html

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 var main = function () {
+    
     "use strict";
-    console.log("hello, world!");
+//    console.log("hello, world!");
+    
+    $(".tabs a:nth-child(1)").on("click", function () {
+        //Делаем все вкладки неактивными
+        $(".tabs span").removeClass("active");
+        //Делаем активной первую вкладку
+        $(".tabs a:nth-child(1) span").addClass("active");
+        //Очищаем основное содержание, чтобы переопределить его
+        $("main .content").empty();
+        //Возвращается false, так как мы не переходим по ссылке
+        return false;
+    });
+    
+    $(".tabs a:nth-child(2)").on("click", function () {
+        $(".tabs span").removeClass("active");
+        $(".tabs a:nth-child(2) span").addClass("active");
+        $("main .content").empty();
+        return false;
+    });
+    
+    $(".tabs a:nth-child(3)").on("click", function () {
+        $(".tabs span").removeClass("active");
+        $(".tabs a:nth-child(3) span").addClass("active");
+        $("main .content").empty();
+        return false;
+    });
+    
 };
 $(document).ready(main);
